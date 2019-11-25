97699626cb8dbf09a86f1258abc3f27caffefe3d
wongzy <wongzhenyu96@gmail.com>
2019-11-25 17:39:39 +0800

    edit adapter

--- recyclerwheelviewlib/src/main/java/cn/wongzhenyu/recyclerwheelview/RecyclerWheelView.kt
+++ recyclerwheelviewlib/src/main/java/cn/wongzhenyu/recyclerwheelview/RecyclerWheelView.kt
@@ -11,21 +11,20 @@ class RecyclerWheelView : RecyclerView {
 
      }
 
-}
+    abstract class Adapter<VH : ViewHolder>: RecyclerView.Adapter<ViewHolder> {
+        constructor() {
+        }
 
-abstract class Adapter: RecyclerView.Adapter<RecyclerView.ViewHolder> {
-    constructor() {
-    }
+        override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): VH {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
 
-    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): RecyclerView.ViewHolder {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
+        override fun getItemCount(): Int {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
 
-    override fun getItemCount(): Int {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-
-    override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
+            TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
+        }
     }
 }
